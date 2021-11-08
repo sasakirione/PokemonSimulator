9334ee1610f3f610587131be9ed83e48e0708b11
sasakirione <sasakirione@gmail.com>
2021-11-09 06:20:20 +0900

    Add: 一部性格の実装
    げきりゅう系、トランジスタ、りゅうのあぎと、ちからもちを実装した

--- src/main/kotlin/com/sasakirione/pokemon/simulator/domain/value/dynamic/Dynamic.kt
+++ src/main/kotlin/com/sasakirione/pokemon/simulator/domain/value/dynamic/Dynamic.kt
@@ -1,17 +1,41 @@
 package com.sasakirione.pokemon.simulator.domain.value.dynamic
 
-class Dynamic(maxHP: Int) {
+import com.sasakirione.pokemon.simulator.domain.value.nature.TypeSelect
+
+/**
+ * ポケモンの状況を担当するクラス
+ *
+ * @constructor 各値を設定します
+ *
+ * @param maxHP 最大HP
+ * @param abilityName 特性の名前
+ */
+class Dynamic(maxHP: Int, abilityName: String) {
+    /** HP */
     private val hp: HP
+    /** 状態異常 */
     private var statusAilment: StatusAilment
+    /** 特性 */
+    private var ability: Ability
 
     init {
         hp = HP(maxHP)
         statusAilment = StatusAilment(StatusAilmentType.NONE)
+        ability = Ability(abilityName)
     }
 
+    /**
+     * 攻撃にかかる補正値を返します
+     *
+     * @return 攻撃にかかる補正値倍率
+     */
+    fun getAttackCorrection(): Double = ability.getAttackCorrection() * statusAilment.getAttackCorrection()
 
-    fun getAttackCorrection(): Double = when (statusAilment.statusAilment) {
-        StatusAilmentType.BURNED -> 0.5
-        else -> 1.0
-    }
+    /**
+     * 技のダメージにかかる補正を返します
+     *
+     * @param moveType 技のタイプ
+     * @return 技のダメージの補正倍率値
+     */
+    fun getMoveDamageCorrect(moveType: TypeSelect): Double = ability.getMoveDamageCorrect(moveType, hp.isHPHalf())
 }
