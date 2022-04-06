72d93d759e14d41997f1b8088a989231e5f71942
y_yamada <yamada03@lri.jp>
2022-04-06 16:18:15 +0900

    Add: 定数クラスの追加
    よく使うぶっぱ努力値を定数クラスに定義した

---
+++ src/main/kotlin/com/sasakirione/pokemon/simulator/const/EffortConst.kt
@@ -0,0 +1,10 @@
+package com.sasakirione.pokemon.simulator.const
+
+object EffortConst {
+    val CS = arrayOf(4, 0, 0, 252, 0, 252)
+    val AS = arrayOf(4, 252, 0, 0, 0, 252)
+    val HC = arrayOf(252, 0, 0, 252, 0, 4)
+    val HA = arrayOf(252, 252, 0, 0, 0, 4)
+    val HB = arrayOf(252, 0, 252, 0, 0, 4)
+    val HD = arrayOf(252, 0, 0, 0, 252, 4)
+}
