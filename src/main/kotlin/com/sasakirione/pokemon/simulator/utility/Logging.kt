8d9844de4b874ec6c167c236249a1019fa0429ca
sasakirione <sasakirione@gmail.com>
2022-04-08 20:09:07 +0900

    Add: Logging method maintained.

--- src/main/kotlin/com/sasakirione/pokemon/simulator/utility/Logging.kt
+++ src/main/kotlin/com/sasakirione/pokemon/simulator/utility/Logging.kt
@@ -1,4 +1,31 @@
 package com.sasakirione.pokemon.simulator.utility
 
 object Logging {
+    private var log = mutableListOf<String>()
+
+    fun getAllLog() {
+        log.forEach{ s -> println(s) }
+    }
+
+    fun getLatestLog() {
+        println( log.last() )
+    }
+
+    fun putPokemon(pokemonName: String, trainerName: String) {
+        log.add("$trainerName は $pokemonName を繰り出した！")
+    }
+
+    fun attack(pokemonName: String, moveName: String) {
+        log.add("$pokemonName の $moveName のこうげきだ！")
+    }
+
+    fun typeMagnification(modification: Double) {
+        val str = when {
+            modification == 0.0 -> "こうかがないようだ"
+            modification < 1 -> "こうかいまひとつだ！"
+            modification > 1 -> "こうかばつぐんだ！"
+            else -> return
+        }
+        log.add(str)
+    }
 }
